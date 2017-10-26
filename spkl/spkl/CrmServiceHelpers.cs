d319bfedf12c311a182c41ef62b9c50ef91b137b
Carlton Colter <carlcolt@microsoft.com>
2017-10-26 16:32:49 -0400

    Updated to properly handle LDAP UPN issues so that if it fails it will prompt for a username.

--- spkl/spkl/CrmServiceHelpers.cs
+++ spkl/spkl/CrmServiceHelpers.cs
@@ -646,33 +646,52 @@ namespace Microsoft.Crm.Sdk.Samples
                         {
                             password = ConvertToSecureString(config.Credentials.UserName.Password);
                         }
+
+                        credentials.UserName.UserName = userName;
+                        credentials.UserName.Password = ConvertToUnsecureString(password);
+                        return credentials;
                     }
+
                     // For OnlineFederation environments, initially try to authenticate with the current UserPrincipalName
                     // for single sign-on scenario.
-                    else if (config.EndpointType == AuthenticationProviderType.OnlineFederation 
-                        && config.AuthFailureCount == 0 
-                        && !String.IsNullOrWhiteSpace(UserPrincipal.Current.UserPrincipalName))
+                    if (config.EndpointType == AuthenticationProviderType.OnlineFederation
+                        && config.AuthFailureCount == 0)
                     {
-                        config.UserPrincipalName = UserPrincipal.Current.UserPrincipalName;
-                        return null;
-                    }
-                    // Otherwise request username and password.
-                    else
-                    {
-                        config.UserPrincipalName = String.Empty;
-                        if (config.EndpointType == AuthenticationProviderType.LiveId)
-                            Console.Write("\n Enter Microsoft account: ");
-                        else
-                            Console.Write("\n Enter Username: ");
-                        userName = Console.ReadLine();
-                        if (string.IsNullOrWhiteSpace(userName))
+                        //Try to get the current UPN, if it fails, ignore the error and don't get the value.
+                        //Issue 160 - UPN is not always accessible through UserPrincipal.Current.
+                        string upn = null;
+
+                        try
                         {
+                            upn = UserPrincipal.Current.UserPrincipalName;
+                        }
+                        catch (Exception ex)
+                        {
+                            upn = null;
+                        }
+                        if (!String.IsNullOrWhiteSpace(upn))
+                        {
+                            config.UserPrincipalName = upn;
                             return null;
                         }
-
-                        Console.Write(" Enter Password: ");
-                        password = ReadPassword();
                     }
+                    
+                    // Otherwise request username and password.
+                    config.UserPrincipalName = String.Empty;
+                    if (config.EndpointType == AuthenticationProviderType.LiveId)
+                        Console.Write("\n Enter Microsoft account: ");
+                    else
+                        Console.Write("\n Enter Username: ");
+                    userName = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(userName))
+                    {
+                        return null;
+                    }
+
+                    Console.Write(" Enter Password: ");
+                    password = ReadPassword();
+                    
+                    
                     credentials.UserName.UserName = userName;
                     credentials.UserName.Password = ConvertToUnsecureString(password);
                     break;                    
