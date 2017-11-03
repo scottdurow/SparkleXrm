24deca2bc32269391e737dae8a970117dd2ace96
Scott Durow <scott_durow@hotmail.com>
2017-11-03 18:09:32 +0000

    Updated to v9 SDK assemblies and .NET 4.6.2

---
+++ spkl/SparkleXrm.Tasks.Tests/DownloadWebresourceFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SparkleXrm.Tasks.Tests
+{
+    [TestClass]
+    public class DownloadWebresourceFiles
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
