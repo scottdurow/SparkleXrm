26cb033c9ed224180dfa36acb87da6fd8afb68ae
Anton Kurnitzky <plantoschka@gmail.com>
2018-01-23 14:19:13 +0100

    Update RollupRequest.cs
    Removed whitespaces

--- SparkleXrmSource/SparkleXrm/Sdk/Messages/RollupRequest.cs
+++ SparkleXrmSource/SparkleXrm/Sdk/Messages/RollupRequest.cs
@@ -17,26 +17,26 @@ namespace Xrm.Sdk.Messages
         public string Serialise()
         {
             return String.Format("<request i:type=\"b:RollupRequest\" xmlns:a=\"http://schemas.microsoft.com/xrm/2011/Contracts\" xmlns:b=\"http://schemas.microsoft.com/crm/2011/Contracts\">"
-            + "        <a:Parameters xmlns:c=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\">"
-            + "        <a:KeyValuePairOfstringanyType>"
-            + "            <c:key>Target</c:key>"
-            + "            <c:value i:type=\"a:EntityReference\">"
-            + "              <a:Id>" + TargetGuid + "</a:Id>"
-            + "              <a:LogicalName>" + TargetLogicalName + "</a:LogicalName>"
-            + "              <a:Name i:nil=\"true\" />"
-            + "            </c:value>"
-            + "          </a:KeyValuePairOfstringanyType>"
-            + "        <a:KeyValuePairOfstringanyType>"
-            +           Query
-            + "        </a:KeyValuePairOfstringanyType>"
-            + "        <a:KeyValuePairOfstringanyType>"
-            + "            <c:key>RollupType</c:key>"
-            + "            <c:value i:type=\"b:RollupType\">" + RollupType + "</c:value>"
-            + "          </a:KeyValuePairOfstringanyType>"
-            + "        </a:Parameters>"
-            + "        <a:RequestId i:nil=\"true\" />"
-            + "        <a:RequestName>Rollup</a:RequestName>"
-            + "      </request>");
+            + "<a:Parameters xmlns:c=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\">"
+            + "<a:KeyValuePairOfstringanyType>"
+            + "<c:key>Target</c:key>"
+            + "<c:value i:type=\"a:EntityReference\">"
+            + "<a:Id>" + TargetGuid + "</a:Id>"
+            + "<a:LogicalName>" + TargetLogicalName + "</a:LogicalName>"
+            + "<a:Name i:nil=\"true\" />"
+            + "</c:value>"
+            + "</a:KeyValuePairOfstringanyType>"
+            + "<a:KeyValuePairOfstringanyType>"
+            + Query
+            + "</a:KeyValuePairOfstringanyType>"
+            + "<a:KeyValuePairOfstringanyType>"
+            + "<c:key>RollupType</c:key>"
+            + "<c:value i:type=\"b:RollupType\">" + RollupType + "</c:value>"
+            + "</a:KeyValuePairOfstringanyType>"
+            + "</a:Parameters>"
+            + "<a:RequestId i:nil=\"true\" />"
+            + "<a:RequestName>Rollup</a:RequestName>"
+            + "</request>");
         }
     }
 }
