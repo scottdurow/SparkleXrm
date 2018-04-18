d6e61776082ce6df860edfb861fc832088cbc2f4
ekhor.asemota <ekhor.asemota@nhsbt.nhs.uk>
2018-04-18 17:17:42 +0100

    re-implemented CrmSvcutil output file manipulation to use Regex rather than Roslyn. As a result, the source code splitting is now done within SparkleXrm.Tasks and there is no longer any need for a separate project for this.

---
+++ spkl/SparkleXrm.Tasks/CrmSvcUtilHelper/SourceCodeRegexUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SparkleXrm.Tasks.CrmSvcUtilHelper
+{
+    public class SourceCodeRegexUtility
+    {
+        private const string ClassPattern = @"([a-zA-Z0-9\(\"",\s\.\)\]\s\n\[:])+public\spartial[a-zA-Z0-9\s:\.,_]+{(?:[^{}]|(?<open>{)|(?<-open>}))+(?(open)(?!))}";
+        private const string EnumPattern = @"([a-zA-Z0-9\(\"",\s\.\)\]\s\n\[:])+public\senum[a-zA-Z0-9\s_]+{(?:[^{}]|(?<open>{)|(?<-open>}))+(?(open)(?!))}";
+
+        public List<TypeContainer> ExtractTypes(string input)
+        {
+            var result = new List<TypeContainer>();
+
+            var regex = new Regex(ClassPattern);
+            var matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                // Console.WriteLine("\n**********************");
+                // Console.WriteLine(match.Value);
+                //  Console.WriteLine("**********************\n\n");
+                result.Add(new TypeContainer(ContainerType.ClassContainer, match.Value));
+            }
+
+            regex = new Regex(EnumPattern);
+            var matches2 = regex.Matches(input);
+            foreach (Match match in matches2)
+            {
+                // Console.WriteLine("\n**********************");
+                //  Console.WriteLine(match.Value);
+                //  Console.WriteLine("**********************\n\n");
+                result.Add(new TypeContainer(ContainerType.EnumContainer, match.Value));
+            }
+
+            return result;
+        }
+    }
+}
