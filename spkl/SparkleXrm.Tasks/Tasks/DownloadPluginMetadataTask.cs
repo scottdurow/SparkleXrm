e07262d48a71c0be89fe839117382624e93335d2
Matt Parks <matt@parks65.com>
2018-10-09 05:51:39 -0500

    Added logic to retain current encoding of source files when writing the updated .cs file. (#278)

--- spkl/SparkleXrm.Tasks/Tasks/DownloadPluginMetadataTask.cs
+++ spkl/SparkleXrm.Tasks/Tasks/DownloadPluginMetadataTask.cs
@@ -53,7 +53,7 @@ namespace SparkleXrm.Tasks
                     if (parser.PluginCount > 0)
                     {
                         // Backup 
-                        File.WriteAllText(parser.FilePath + DateTime.Now.ToString("yyyyMMddHHmmss") + ".bak", parser.Code);
+                        File.WriteAllText(parser.FilePath + DateTime.Now.ToString("yyyyMMddHHmmss") + ".bak", parser.Code, parser.CurEncoding);
                         foreach (var pluginType in parser.ClassNames)
                         {
                             // Remove existing attributes
@@ -73,7 +73,7 @@ namespace SparkleXrm.Tasks
                             }
                         }
                         // Update 
-                        File.WriteAllText(parser.FilePath, parser.Code);
+                        File.WriteAllText(parser.FilePath, parser.Code, parser.CurEncoding);
                         codeFilesUpdated++;
                     }
                 }
