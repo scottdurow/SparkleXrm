a11fee57b31e5e5811ffb1ee5842eb4d6b93aa75
Corey Sutton <coreysutton@outlook.co.nz>
2019-05-08 08:20:46 +1200

    Add tests for CodeParser.AddAttribute()

---
+++ spkl/SparkleXrm.Tasks.Tests/CodeParserTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SparkleXrm.Tasks;
+using System.Text;
+
+namespace SparkleXrm.Tasks.Tests
+{
+    [TestClass]
+    public class CodeParserTests
+    {
+        private CrmPluginRegistrationAttribute _crmPluginRegistrationAttr = new CrmPluginRegistrationAttribute(
+                "Create",
+                "opportunity",
+                StageEnum.PostOperation,
+                ExecutionModeEnum.Synchronous,
+                "",
+                "PostOpportunityCreate",
+                1,
+                IsolationModeEnum.Sandbox);
+
+        [TestMethod]
+        [TestCategory("Unit Tests")]
+        public void AddAttributeTest_SingleLine()
+        {
+            var pluginDefinitionTemplate = "namespace MyCompany.Plugins{{{0}public class OpportunityPlugin : Plugin {{ }} }}";
+            var code = string.Format(pluginDefinitionTemplate, "");
+            
+            var codeParser = new CodeParser(code);
+            codeParser.AddAttribute(_crmPluginRegistrationAttr, "MyCompany.Plugins.OpportunityPlugin");
+
+            var expectedPluginDeifintionSB = string.Format(pluginDefinitionTemplate, _crmPluginRegistrationAttr.GetAttributeCode("\r\n") + "\r\n");
+
+            Assert.AreEqual(codeParser.Code, expectedPluginDeifintionSB.ToString());
+        }
+
+        [TestMethod]
+        [TestCategory("Unit Tests")]
+        public void AddAttributeTest_MultiLine()
+        {
+            var pluginDefinitionTemplate = "namespace MyCompany.Plugins {{{0}\r\n    public class OpportunityPlugin : Plugin {{\r\n    }}\r\n}}";
+            var code = string.Format(pluginDefinitionTemplate, "");
+
+            var codeParser = new CodeParser(code);
+            codeParser.AddAttribute(_crmPluginRegistrationAttr, "MyCompany.Plugins.OpportunityPlugin");
+
+            var expectedPluginDeifintionSB = string.Format(pluginDefinitionTemplate, _crmPluginRegistrationAttr.GetAttributeCode("\r\n    "));
+
+            Assert.AreEqual(codeParser.Code, expectedPluginDeifintionSB.ToString());
+        }
+    }
+}
