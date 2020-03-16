0185c033ad2a5d45cd127fc2301bb3f08912e0cc
Teppo Lainio <teppo.lainio@gmail.com>
2020-03-16 13:08:19 +0200

    Corrected log messages to log correct task behaviour.

--- spkl/SparkleXrm.Tasks/Tasks/DeployPluginsAndWorkflowTask.cs
+++ spkl/SparkleXrm.Tasks/Tasks/DeployPluginsAndWorkflowTask.cs
@@ -22,7 +22,7 @@ namespace SparkleXrm.Tasks {
 
         protected override void ExecuteInternal(string folder, OrganizationServiceContext ctx)
         {
-            _trace.WriteLine("Searching for plugin config in '{0}'", folder);
+            _trace.WriteLine("Searching for plugin and workflow config in '{0}'", folder);
             var configs = ServiceLocator.ConfigFileFactory.FindConfig(folder);
 
             foreach (var config in configs)
